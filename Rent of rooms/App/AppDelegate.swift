64c2a4dec8444566fd718ec3392657b7047270aa
SolnyshkinSM <SolnyshkinSM@gmail.com>
2020-10-21 15:03:52 +0300

    add space

--- Rent of rooms/App/AppDelegate.swift
+++ Rent of rooms/App/AppDelegate.swift
@@ -11,7 +11,8 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-        
+      
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         UINavigationBar.appearance().tintColor = .black
